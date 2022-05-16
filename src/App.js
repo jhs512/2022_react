848fbfb5c6e0fe40399f6a6bcefb3f65aefcba13
jhs512 <jangka512@gmail.com>
2022-05-16 20:26:57 +0900

    폰트어썸 추가

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import { Button, Chip } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="contained">하이</Button>
-      <hr className="my-5" />
-      <span className="font-bold text-3xl">
-        <span className="text-[color:var(--mui-color-primary-main)]">안녕</span>
-        하세요.
-      </span>
-      <hr className="my-5" />
-      <Chip label="Chip Filled" className="!pt-1" />
+    <div className="flex-1 flex items-center justify-center">
+      <div>
+        <Button variant="contained">하이</Button>
+        <hr className="my-5" />
+        <span className="font-bold text-3xl">
+          <span className="text-[color:var(--mui-color-primary-main)]">
+            안녕
+          </span>
+          하세요.
+        </span>
+        <hr className="my-5" />
+        <Chip
+          label={
+            <span>
+              <i className="fa-solid fa-bell"></i> Chip Filled
+            </span>
+          }
+          className="!pt-1"
+        />
+      </div>
     </div>
   );
 }
