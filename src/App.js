12baa876865082778f04af23cb21d931227b7747
jhs512 <jangka512@gmail.com>
2022-05-16 17:29:58 +0900

    MUI 설치

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { Button } from "@mui/material";
+
 function App() {
   return (
     <div className="App">
+      <Button variant="contained">하이</Button>
+      <br />
       <span className="font-bold text-3xl">안녕하세요.</span>
     </div>
   );
